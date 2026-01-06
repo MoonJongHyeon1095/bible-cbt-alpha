a08c34195af341b6030e5e3eb3405705a51751c2
JongHyeon Moon <zin354@gmail.com>
2026-01-06 17:33:15 +0900

    feat: initiate Capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.cbt',
+  appName: 'Cognitive Behavioral Therapy Tool',
+  webDir: 'dist'
+};
+
+export default config;
